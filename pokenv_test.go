a5fb6f7bf2090518d2eeb5d54861997a08dd8893
Daniel TISCHER <dos.7182@gmail.com>
2015-06-08 13:26:25 +0200

    more tests

--- pokenv_test.go
+++ pokenv_test.go
@@ -11,7 +11,7 @@ func init() {
 	log.SetOutput(ioutil.Discard)
 }
 
-func TestProcessValue(t *testing.T) {
+func TestProcessLineValue(t *testing.T) {
 	environment = make(map[string][]string)
 	currentVariable = "TESTING"
 	addCurrent("")
@@ -20,6 +20,30 @@ func TestProcessValue(t *testing.T) {
 
 	expected := "value"
 	actual := environment[currentVariable][0]
+
+	if actual != expected {
+		t.Errorf("Expected: %q, was: %q", expected, actual)
+	}
+}
+
+func TestProcessLineSection(t *testing.T) {
+	environment = make(map[string][]string)
+
+	processLine("[ A SECTION ]")
+
+	expected := "ASECTION"
+	actual := currentVariable
+
+	if actual != expected {
+		t.Errorf("Expected: %q, was: %q", expected, actual)
+	}
+}
+
+func TestProcessTestFile(t *testing.T) {
+	setEnv(HKLM, `data/setvar.txt`)
+
+	expected := "valueline1"
+	actual := mock.env["POKE_SECTION"]
 
 	if actual != expected {
 		t.Errorf("Expected: %q, was: %q", expected, actual)
